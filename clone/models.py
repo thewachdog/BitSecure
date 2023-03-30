4f608e8cd4f93bb5207f2586678457e72d43ec7e
barat <barat@localhost.localdomain>
2023-03-30 21:38:20 +0530

    user creation finished

--- clone/models.py
+++ clone/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username=models.CharField(max_length = 250)
+    email=models.CharField(max_length = 250)
+    firstname=models.CharField(max_length = 250)
+    lastname=models.CharField(max_length = 250)
+    password=models.CharField(max_length = 250)
+    plan=models.CharField(max_length = 250)
--- clone/views.py
+++ clone/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html", {})
+    if request.POST:
+        print(request.POST)
+        return render(request, "index.html", {})
+    else:
+        return render(request, "index.html", {})
 
 def login(request):
+    if request.POST:
+        print(request.POST)
+        a = User(username = request.POST['username'], email = request.POST['email'], firstname = request.POST['firstname'], lastname = request.POST['lastname'], password = request.POST['password'], plan = request.POST['plan'])
+        a.save()
     return render(request, "login.html", {})
 
 def signup(request):
